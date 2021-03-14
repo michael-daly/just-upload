7b3eb2e5890cb78900e48b18af0008024fcc632d
Michael <mjdaly10@gmail.com>
2021-03-14 14:08:47 -0700

    Restyled UploadSuccessView component
    + Added deletion link copying functionality with copy-to-clipboard package
    + Added button-transition CSS class for transitioning between button colors

--- src/client/js/upload/UploadSuccessView.jsx
+++ src/client/js/upload/UploadSuccessView.jsx
@@ -1,10 +1,35 @@
 import m from 'mithril';
+import copy from 'copy-to-clipboard';
 
 import UploadModel from '~/upload/UploadModel.js';
 
 
+const COPY_TIMEOUT = 1100;
+
+
 const UploadSuccessView =
 {
+	oninit ()
+	{
+		this.copyTimeout = 0;
+	},
+
+	copyLink ( link )
+	{
+		if ( this.copyTimeout <= 0)
+		{
+			copy (window.location.host + link);
+
+			const self = this;
+
+			this.copyTimeout = setTimeout (() =>
+			{
+				self.copyTimeout = 0;
+				m.redraw ();
+			}, COPY_TIMEOUT);
+		}
+	},
+
 	view ( component )
 	{
 		const { downloadKey = null, deleteKey = null } = component.attrs;
@@ -22,32 +47,38 @@ const UploadSuccessView =
 			deleteLink = `/delete/${downloadKey}?deleteKey=${deleteKey}`;
 		}
 
+		const isCopying = this.copyTimeout > 0;
+		const self = this;
+
 		return (
 			<div>
-				<div>
-					<strong>Upload Successful!</strong>
-				</div>
-				{
-					downloadLink === '' ? '' :
-					<div>
+				<div class='columns is-centered pt-2'>
+					<div class='column is-narrow'>
 						<label>Download Link: </label>
 						<a href={downloadLink}>{window.location.host + downloadLink}</a>
 					</div>
-				}
-				{
-					deleteLink === '' ? '' :
-					<div>
-						<label>Deletion Link: </label>
-						<a href={deleteLink}>{window.location.host + deleteLink}</a>
-					</div>
-				}
+				</div>
 
-				<div>
-					<input
-						type='button'
-						value='Upload Another File'
-						onclick={() => UploadModel.subview = 'upload'}
-					/>
+				<hr class='mt-2 ml-6 mr-6' />
+
+				<div class='columns is-centered pb-3'>
+					<div class='column is-narrow'>
+						<button
+							class={'button button-transition ' + (isCopying ? '' : 'is-danger')}
+							onclick={() => self.copyLink (deleteLink)}
+							disabled={isCopying}
+						>
+							{isCopying ? 'Copied' : 'Copy Deletion Link'}
+						</button>
+					</div>
+					<div class='column is-narrow'>
+						<button
+							class='button is-primary'
+							onclick={() => UploadModel.subview = 'upload'}
+						>
+							Upload Another File
+						</button>
+					</div>
 				</div>
 			</div>
 		);
