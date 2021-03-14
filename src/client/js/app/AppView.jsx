bd8e2bd3271e29bd6fd43bf5d5f65b4552c677e8
Michael <mjdaly10@gmail.com>
2021-03-14 14:48:56 -0700

    Added logo and subtitle to AppView

--- src/client/js/app/AppView.jsx
+++ src/client/js/app/AppView.jsx
@@ -42,21 +42,36 @@ const AppView =
 			<div>
 				<Navbar />
 
-				<div class='columns is-centered'>
-					<div class='column is-two-fifths'>
-						<div class='box box-primary'>
-							{viewComponent}
+				<section class='section'>
+					<div class='columns is-centered is-vcentered'>
+						<div class='column has-text-centered'>
+							<a onclick={() => m.route.set ('/')}>
+								<img src='./img/logo.png' style={{ height: '8rem' }}>
+									<h1 class='title is-1'>Just Upload</h1>
+								</img>
+							</a>
+							<h4 class='subtitle'>
+								No ads, no sign-ups, no data collection
+							</h4>
 						</div>
 					</div>
-				</div>
 
-				<div class='columns is-centered'>
-					<div class='column is-one-third'>
-						<div class='box'>
-							<RecentFilesView />
+					<div class='columns is-centered is-vcentered'>
+						<div class='column is-two-fifths'>
+							<div class='box box-primary'>
+								{viewComponent}
+							</div>
 						</div>
 					</div>
-				</div>
+
+					<div class='columns is-centered'>
+						<div class='column is-one-third'>
+							<div class='box'>
+								<RecentFilesView />
+							</div>
+						</div>
+					</div>
+				</section>
 			</div>
 		);
 	},
